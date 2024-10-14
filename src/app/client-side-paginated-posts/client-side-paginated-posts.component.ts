c2549605fdde1ed27eb8fe963983771cf7676836
Michael Berger <m.gonser@icloud.com>
2024-10-14 14:29:19 +0200

    include search field

--- src/app/client-side-paginated-posts/client-side-paginated-posts.component.ts
+++ src/app/client-side-paginated-posts/client-side-paginated-posts.component.ts
@@ -1,14 +1,39 @@
 import {Component, inject} from '@angular/core';
 import {DataService} from './data.service';
-import {concatMap, exhaustMap, map, mergeMap, Subject, switchMap} from 'rxjs';
+import {
+  concatMap,
+  debounceTime,
+  distinctUntilChanged,
+  exhaustMap,
+  filter,
+  map,
+  mergeMap,
+  Subject,
+  switchMap
+} from 'rxjs';
 import {AsyncPipe} from '@angular/common';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput, MatInputModule} from '@angular/material/input';
+
+/**
+ * Steps:
+ * idListe und Entitäten laden
+ * Filterung
+ * Paginierung Clientseitig
+ */
 
 @Component({
   selector: 'app-client-side-paginated-posts',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, ReactiveFormsModule, MatFormField, MatInputModule],
   template: `
-    <button (click)="click$.next()">click</button>
+    <mat-form-field >
+      <mat-label>Suche</mat-label>
+      <input matInput [formControl]="searchFormControl">
+    </mat-form-field>
+    Suche: {{searchQuery$ | async}}
+
     @if(data$ | async; as data ){
        @for(post of data; track post.id){
       <div>{{post.title}}</div>
@@ -29,17 +54,20 @@ import {AsyncPipe} from '@angular/common';
 export class ClientSidePaginatedPostsComponent {
    #dataService = inject(DataService)
 
-   click$ = new Subject<void>()
+   searchFormControl = new FormControl('')
+
+   searchQuery$ = this.searchFormControl.valueChanges.pipe(
+    debounceTime(500),
+    filter(Boolean),
+    filter(query => query?.length > 2),
+    distinctUntilChanged(),
+   )
 
    data$ = this.#dataService.getIdList().pipe(
    // exhaustMap, concatMap, mergeMap
     switchMap(ids => this.#dataService.getPostsForIds(ids))
    )
 
-   constructor() {
-    this.#dataService.getIdList().subscribe(idList => {
-      this.#dataService.getPostsForIds(idList).subscribe()
-    })
-   }
+
 
 }
